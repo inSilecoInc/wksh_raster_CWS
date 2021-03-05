d3bd6eca327228ac47a07888704101a5910c9120
Kevin Cazelles <kcazelle@uoguelph.ca>
2021-03-05 18:16:51 -0500

    :pencil: stucture day 1

---
+++ _setup.R
@@ -0,0 +1,26 @@
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  warning = FALSE,
+  message = FALSE,
+  dev = "png",
+  dpi = 108,
+  fig.width = 6,
+  fig.height = 4.5,
+  fig.align = 'center',
+  width = 120
+)
+options(htmltools.dir.version = FALSE, htmltools.preserve.raw = FALSE)
+cdw <- function(...) countdown::countdown(...)
+rfa <- function(...) icons::fontawesome(...)
+emj <- function(...) emo::ji(...)
+kig <- function(...) knitr::include_graphics(...)
+gh <- function() rfa("github")
+rp <- function() rfa("r-project")
+ck <- function() rfa("clock")
+db <- function() rfa("database")
+nf <- function() rfa("info-circle")
+lk <- function() rfa("link")
+bk <- function() rfa("book")
+pd <- function() rfa("file-pdf")
+ar <- function() rfa("arrow-right")
